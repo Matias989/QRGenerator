fe4b2521e00f8ce53321c7ed323f8cd1a7b2016f
matias <matiaspena93@hotmail.com>
2022-08-08 08:30:51 -0300

    First Commit

---
+++ Generador de QR/FormPrincipal.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace Generador_de_QR
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PictureBoxQr = new System.Windows.Forms.PictureBox();
+            this.GenerarBTN = new System.Windows.Forms.Button();
+            this.GuardarBTN = new System.Windows.Forms.Button();
+            this.TextTXT = new System.Windows.Forms.TextBox();
+            this.rd2x2 = new System.Windows.Forms.RadioButton();
+            this.rd5x5 = new System.Windows.Forms.RadioButton();
+            this.rd3x3 = new System.Windows.Forms.RadioButton();
+            this.gbRadioButtons = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxQr)).BeginInit();
+            this.gbRadioButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PictureBoxQr
+            // 
+            this.PictureBoxQr.Location = new System.Drawing.Point(12, 12);
+            this.PictureBoxQr.Name = "PictureBoxQr";
+            this.PictureBoxQr.Size = new System.Drawing.Size(251, 250);
+            this.PictureBoxQr.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBoxQr.TabIndex = 0;
+            this.PictureBoxQr.TabStop = false;
+            // 
+            // GenerarBTN
+            // 
+            this.GenerarBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GenerarBTN.Location = new System.Drawing.Point(12, 345);
+            this.GenerarBTN.Name = "GenerarBTN";
+            this.GenerarBTN.Size = new System.Drawing.Size(120, 35);
+            this.GenerarBTN.TabIndex = 1;
+            this.GenerarBTN.Text = "Generar";
+            this.GenerarBTN.UseVisualStyleBackColor = true;
+            this.GenerarBTN.Click += new System.EventHandler(this.GenerarBTN_Click);
+            // 
+            // GuardarBTN
+            // 
+            this.GuardarBTN.Enabled = false;
+            this.GuardarBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.GuardarBTN.Location = new System.Drawing.Point(143, 345);
+            this.GuardarBTN.Name = "GuardarBTN";
+            this.GuardarBTN.Size = new System.Drawing.Size(120, 35);
+            this.GuardarBTN.TabIndex = 2;
+            this.GuardarBTN.Text = "Guardar";
+            this.GuardarBTN.UseVisualStyleBackColor = true;
+            this.GuardarBTN.Click += new System.EventHandler(this.GuardarBTN_Click);
+            // 
+            // TextTXT
+            // 
+            this.TextTXT.Location = new System.Drawing.Point(12, 268);
+            this.TextTXT.Name = "TextTXT";
+            this.TextTXT.Size = new System.Drawing.Size(250, 20);
+            this.TextTXT.TabIndex = 3;
+            // 
+            // rd2x2
+            // 
+            this.rd2x2.AutoSize = true;
+            this.rd2x2.Checked = true;
+            this.rd2x2.Location = new System.Drawing.Point(6, 19);
+            this.rd2x2.Name = "rd2x2";
+            this.rd2x2.Size = new System.Drawing.Size(45, 17);
+            this.rd2x2.TabIndex = 4;
+            this.rd2x2.Text = "2x2";
+            this.rd2x2.UseVisualStyleBackColor = true;
+            this.rd2x2.CheckedChanged += new System.EventHandler(this.rd2x2_CheckedChanged);
+            // 
+            // rd5x5
+            // 
+            this.rd5x5.AutoSize = true;
+            this.rd5x5.Location = new System.Drawing.Point(203, 19);
+            this.rd5x5.Name = "rd5x5";
+            this.rd5x5.Size = new System.Drawing.Size(45, 17);
+            this.rd5x5.TabIndex = 5;
+            this.rd5x5.Text = "5x5";
+            this.rd5x5.UseVisualStyleBackColor = true;
+            this.rd5x5.CheckedChanged += new System.EventHandler(this.rd5x5_CheckedChanged);
+            // 
+            // rd3x3
+            // 
+            this.rd3x3.AutoSize = true;
+            this.rd3x3.Location = new System.Drawing.Point(101, 19);
+            this.rd3x3.Name = "rd3x3";
+            this.rd3x3.Size = new System.Drawing.Size(45, 17);
+            this.rd3x3.TabIndex = 6;
+            this.rd3x3.Text = "3x3";
+            this.rd3x3.UseVisualStyleBackColor = true;
+            this.rd3x3.CheckedChanged += new System.EventHandler(this.rd3x3_CheckedChanged);
+            // 
+            // gbRadioButtons
+            // 
+            this.gbRadioButtons.Controls.Add(this.rd3x3);
+            this.gbRadioButtons.Controls.Add(this.rd2x2);
+            this.gbRadioButtons.Controls.Add(this.rd5x5);
+            this.gbRadioButtons.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbRadioButtons.Location = new System.Drawing.Point(12, 294);
+            this.gbRadioButtons.Name = "gbRadioButtons";
+            this.gbRadioButtons.Size = new System.Drawing.Size(251, 45);
+            this.gbRadioButtons.TabIndex = 7;
+            this.gbRadioButtons.TabStop = false;
+            this.gbRadioButtons.Text = "Tamaño";
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 389);
+            this.Controls.Add(this.gbRadioButtons);
+            this.Controls.Add(this.TextTXT);
+            this.Controls.Add(this.GuardarBTN);
+            this.Controls.Add(this.GenerarBTN);
+            this.Controls.Add(this.PictureBoxQr);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormPrincipal";
+            this.ShowIcon = false;
+            this.Text = "Generador QR";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxQr)).EndInit();
+            this.gbRadioButtons.ResumeLayout(false);
+            this.gbRadioButtons.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBoxQr;
+        private System.Windows.Forms.Button GenerarBTN;
+        private System.Windows.Forms.Button GuardarBTN;
+        private System.Windows.Forms.TextBox TextTXT;
+        private System.Windows.Forms.RadioButton rd2x2;
+        private System.Windows.Forms.RadioButton rd5x5;
+        private System.Windows.Forms.RadioButton rd3x3;
+        private System.Windows.Forms.GroupBox gbRadioButtons;
+    }
+}
+
---
+++ Generador de QR/FormPrincipal.cs
@@ -0,0 +1,117 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generador_de_QR
+{
+    public partial class FormPrincipal : Form
+    {
+        private enum SizeImage
+        {
+            DosxDos = 0,
+            TresxTres = 1,
+            CincoxCinco = 2
+        }
+
+        private int sizeImage;
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerarBTN_Click(object sender, EventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(TextTXT.Text, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            string pathImg = Path.Combine(Application.StartupPath, "Img\\IconoMultimedioRosario.png");
+            Bitmap qrCodeImage = qrCode.GetGraphic(20, Color.Black, Color.White, (Bitmap)Bitmap.FromFile(pathImg));
+            PictureBoxQr.Image = qrCodeImage;
+            GuardarBTN.Enabled = true;
+        }
+
+        private void GuardarBTN_Click(object sender, EventArgs e)
+        {
+            string pathImgSave = "";
+            Image i = resizeImage(PictureBoxQr.Image, new Size(354, 354));
+
+            if (!System.IO.Directory.Exists(Path.Combine(Application.StartupPath, "GeneratedQR")))
+                Directory.CreateDirectory(Path.Combine(Application.StartupPath, "GeneratedQR"));
+
+            switch (sizeImage)
+            {
+                case (int)SizeImage.DosxDos:
+                    pathImgSave = Path.Combine(Application.StartupPath, "GeneratedQR\\QR - 2x2 .png");
+                    i = resizeImage(PictureBoxQr.Image, new Size(236, 236));
+                    i.Save(pathImgSave, System.Drawing.Imaging.ImageFormat.Png);
+                    break;
+                case (int)SizeImage.TresxTres:
+                    pathImgSave = Path.Combine(Application.StartupPath, "GeneratedQR\\QR - 3x3 .png");
+                    i = resizeImage(PictureBoxQr.Image, new Size(354, 354));
+                    i.Save(pathImgSave, System.Drawing.Imaging.ImageFormat.Png);
+                    break;
+                case (int)SizeImage.CincoxCinco:
+                    pathImgSave = Path.Combine(Application.StartupPath, "GeneratedQR\\QR - 5x5 .png");
+                    i = resizeImage(PictureBoxQr.Image, new Size(591, 591));
+                    i.Save(pathImgSave, System.Drawing.Imaging.ImageFormat.Png);
+                    break;
+            }
+
+            MessageBox.Show("Se genero el QR correctamente");
+        }
+
+        private static System.Drawing.Image resizeImage(System.Drawing.Image imgToResize, Size size)
+        {
+            //Get the image current width  
+            int sourceWidth = imgToResize.Width;
+            //Get the image current height  
+            int sourceHeight = imgToResize.Height;
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+            //Calulate  width with new desired size  
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            //Calculate height with new desired size  
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+            //New Width  
+            int destWidth = (int)(sourceWidth * nPercent);
+            //New Height  
+            int destHeight = (int)(sourceHeight * nPercent);
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((System.Drawing.Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            // Draw image with new width and height  
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+            return (System.Drawing.Image)b;
+        }
+
+        private void rd2x2_CheckedChanged(object sender, EventArgs e)
+        {
+            sizeImage = (int)SizeImage.DosxDos;
+        }
+
+        private void rd3x3_CheckedChanged(object sender, EventArgs e)
+        {
+            sizeImage = (int)SizeImage.TresxTres;
+        }
+
+        private void rd5x5_CheckedChanged(object sender, EventArgs e)
+        {
+            sizeImage = (int)SizeImage.CincoxCinco;
+        }
+    }
+}
